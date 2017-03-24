fba102e5b33b4fed4fb176d8cc2e6c97d6b8f97b
darshan281996 <darshantrivedi28@gmail.com>
2017-03-24 11:55:01 +0530

    [imad-console] Updates ui/page1.js

--- ui/page1.js
+++ ui/page1.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 var save = document.getElementById("save_button");
 save.addEventListener('click',function(){
     var comment=document.getElementById('comment_textbox');
-   var enter= comment.getInnerHTML;
+   var enter= comment.value;
    alert(enter);
 });
