8009ff0de2a0a1c404bada5d5188cb10eb610090
darshan281996 <darshantrivedi28@gmail.com>
2017-03-24 12:18:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,17 @@ var button=document.getElementById("page1_button");
 button.addEventListener("click",function(){
     window.location.href="page1";
 });
+var response;
+var comment=document.getElementById('lastcomment');
+var request=new XMLHttpRequest();
+request.onreadystatechange=function(){
+  if(request.readyState===XMLHttpRequest.Done){
+      if(request.status===200){
+          response=request.responseText;
+          comment.innerHTML=response;
+      }
+      
+  }  
+   request.open('GET',"http://darshan281996.cloud.imad.hasura-app.io/comment/hello",true);
+   request.send(null);
+};
