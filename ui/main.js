945795571448b22e28a0a3042ef4645065d30f20
darshan281996 <darshantrivedi28@gmail.com>
2017-03-24 11:47:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ var button=document.getElementById("page1_button");
 button.addEventListener("click",function(){
     window.location.href="page1";
 });
+var save = document.getElementById("save_button");
+save.addEventListener('click',function(){
+    var comment=document.getElementById('comment_textbox');
+   var enter= comment.getInnerHTML;
+   alert(enter);
+});
